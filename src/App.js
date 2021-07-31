5aa7dbd56b93a45176f9701d1f845191d16310a2
Robson Rocha <robsonrocha@pop-os.localdomain>
2021-07-31 19:39:52 -0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import historia from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
@@ -9,11 +11,13 @@ import Routes from './routes';
 
 export default function App() {
   return (
-    <Router history={historia}>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} className="toast-container" />
-    </Router>
+    <Provider store={store}>
+      <Router history={historia}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </Router>
+    </Provider>
   );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 import axios from '../../services/axios';
 
 export default function Login() {
-  React.useEffect(() => {
-    async function getData() {
-      const response = await axios.get('/funcionarios');
-      console.log(response);
-    }
+  const dispatch = useDispatch();
+
+  async function getData() {
+    const response = await axios.get('/funcionarios');
+    console.log(response);
+  }
+
+  const handleClick = (e) => {
     getData();
-  }, []);
+    e.preventDefault();
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  };
 
   return (
     <Container>
@@ -21,7 +29,9 @@ export default function Login() {
       </Title>
       <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
 
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
